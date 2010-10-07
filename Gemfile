57d3ec2f64a7c50c54824131f17a8310d3999f21
Abhishek Yadav <abhishek@tenmiles.com>
2010-10-08 00:24:28 +0530

    changed the directory structure

---
+++ Gemfile
@@ -0,0 +1,34 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+#gem 'rdiscount', :require => 'rdiscount'  # Markdown only. C binary. Fast
+#gem 'RedCloth', :require => 'RedCloth'    # Textile. C binary. Fast
+gem 'kramdown', :require => 'kramdown'    # MArkdown extensions. Pure ruby. Slow
+gem 'pdfkit', :require => 'pdfkit'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug'
+
+# Bundle the extra gems:
+# gem 'bj'
+# gem 'nokogiri'
+# gem 'sqlite3-ruby', :require => 'sqlite3'
+# gem 'aws-s3', :require => 'aws/s3'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+require 'config/deploy/capistrano_database_yml'
+
+set :application, "rc"
+set :deploy_to, "/var/www/rc3"
+set :user, "root"
+set :runner, user
+set :apache_site_folder, "/etc/apache2/sites-enabled"
+
+set :scm, :git
+set :repository,  "git@github.com:zerothabhishek/rc3.git"
+set :branch, "master"
+set :scm_passphrase, Capistrano::CLI.password_prompt("Enter your git passphrase: ")
+set :deploy_via, :remote_cache
+
+default_run_options[:pty] = true  # Must be set for the password prompt from git to work
+
+set :app_site, "li161-110.members.linode.com" 
+role :web, app_site                           # Your HTTP server, Apache/etc
+role :app, app_site                           # This may be the same as your `Web` server
+role :db,  app_site, :primary => true         # This is where Rails migrations will run
+#role :db,  ""                                 # your slave db-server here
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
